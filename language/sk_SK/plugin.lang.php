5ef9c71405827c451c8684755b8a0a1e1b982a62
ddtddt <ddtddt@piwigo.org>
2011-12-30 09:30:06 +0000

    [extensions] - GThumb - add sk_SK – Slovensky – Slovak - Thanks to dodo
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@12806 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['Thumbnails max height'] = 'Maximálna výška náhľadov';
+$lang['Margin between thumbnails'] = 'Odstup medzi náhľadmi';
+$lang['Double the size of the first thumbnail'] = 'Dvojitá veľkosť prvého náhľadu';
+$lang['Cache the big thumbnails (recommended)'] = 'Vyrovnávacia pamäť veľých náhľadov (odporúčané)';
+$lang['Scale thumbnails'] = 'Škála náhľadov';
+$lang['Cache Informations'] = 'Informácie o vyrovnávacej pamäti';
+$lang['Purge thumbnails cache'] = 'Vyčistenie vyrovnávacej pamäti náhľadov';
+$lang['%d file'] = '%d súbor';
+$lang['%d files'] = '%d súborov';
+$lang['Cache have been generated'] = 'Vytváranie vyrovnávacej pamäti';
+$lang['Pre-cache thumbnails'] = 'Predvytvorenie vyrovnávacej pamäti náhľadov';
+$lang['Delete images in GThumb+ cache.'] = 'Vymazanie fotiek v GThumb+ vyrovnávacej pamäti.';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Vyhľadanie fotiek, ktoré nie sú vo vyrovnávacej pamäti a vytvorenie ich verzie.';
+
+?>
