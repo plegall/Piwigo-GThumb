2bf19b30f551a49247e73f3e8dde6986768a8e20
ddtddt <ddtddt@piwigo.org>
2011-12-30 09:39:33 +0000

    [extensions] - GThumb - add pl_PL - Polski - Polish - Thanks to T00kie
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@12807 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['Thumbnails max height'] = 'Maksymalna wysokość miniatur';
+$lang['Margin between thumbnails'] = 'Margines między miniaturami';
+$lang['Double the size of the first thumbnail'] = 'Podwój rozmiar pierwszej miniatury';
+$lang['Cache the big thumbnails (recommended)'] = 'Umieść w pamięci podręcznej duże miniatury (rekomendowane)';
+$lang['Scale thumbnails'] = 'Skaluj miniatury';
+$lang['Cache Informations'] = 'Umieść w pamięci podręcznej informacje';
+$lang['Purge thumbnails cache'] = 'Wyczyść pamięć podręczną miniatur';
+$lang['%d file'] = '%d plik';
+$lang['%d files'] = '%d pliki';
+$lang['Cache have been generated'] = 'Pamięć podręczna została utworzona';
+$lang['Pre-cache thumbnails'] = 'Wywołuj miniatury z pamięci podręcznej';
+$lang['Delete images in GThumb+ cache.'] = 'Usuń zdjęcia z pamięci podręcznej GThumb+ .';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Znajduje zdjęcia, które nie zostały umieszczone w pamięci podręcznej i tworzy tam ich kopie.';
+
+?>
