80e4c8ebf38dcc15241c090077918ac6772f2039
ddtddt <ddtddt@piwigo.org>
2011-12-30 09:20:25 +0000

    [extensions] - GThumb - add lv_LV - Latviešu - Latvian - Thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@12804 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['Thumbnails max height'] = 'Sīkbilžu maksimālais augstums';
+$lang['Margin between thumbnails'] = 'Atstarpe starp sīkbildēm';
+$lang['Double the size of the first thumbnail'] = 'Dubultot pirmās sīkbildes izmēru';
+$lang['Cache the big thumbnails (recommended)'] = 'Izmantot Cache atmiņu (kešot) lielajām sīkbildēm (ieteicams)';
+$lang['Scale thumbnails'] = 'Mērogot sīkbildes';
+$lang['Cache Informations'] = 'Izmantot Cache Informācijai';
+$lang['Purge thumbnails cache'] = 'Iztīrīt sīkbilžu cache atmiņu';
+$lang['%d file'] = '%d fails';
+$lang['%d files'] = '%d faili';
+$lang['Cache have been generated'] = 'Cache atmiņa izveidota';
+$lang['Pre-cache thumbnails'] = 'Kešot sīkbildes';
+$lang['Delete images in GThumb+ cache.'] = 'Dzēst attēlus iekš GThumb+ cache.';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Atrod attēlos, kas vēl nav kešoti un izveido kešotu versiju.';
+
+?>
