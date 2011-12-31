87e19590bcc92969c29307f1f199a1f42f545a27
ddtddt <ddtddt@piwigo.org>
2011-12-31 12:56:27 +0000

    [extensions] - GThumb - add zh_CN - 简体中文 - Chinese - Thanks to quqibing
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@12814 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['Thumbnails max height'] = '缩略图最大高度';
+$lang['Margin between thumbnails'] = '缩略图间距';
+$lang['Double the size of the first thumbnail'] = '第一张缩略图为两倍大小';
+$lang['Cache the big thumbnails (recommended)'] = '缓存大缩略图(推荐)';
+$lang['Scale thumbnails'] = '缩放缩略图方式';
+$lang['Cache Informations'] = '缓存信息';
+$lang['Purge thumbnails cache'] = '清空缩略图缓存';
+$lang['%d file'] = '%d 个文件';
+$lang['%d files'] = '%d 个文件';
+$lang['Cache have been generated'] = '缓存生成完毕';
+$lang['Pre-cache thumbnails'] = '预缓存缩略图';
+$lang['Delete images in GThumb+ cache.'] = '删除GThumb+缓存中的图片.';
+$lang['Finds images that have not been cached and creates the cached version.'] = '某些图片无法被缓存并创建缓存版本.';
+
+?>
