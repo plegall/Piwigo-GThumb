feaf0d92438c7fad1466bb159ea35b0e962aec94
ddtddt <ddtddt@piwigo.org>
2012-01-15 17:24:28 +0000

    [extensions] - GThumb - add ru_RU - Русский - Russian - Thanks to yuragal
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@12897 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['Thumbnails max height'] = 'Максимальная высота миниатюры';
+$lang['Margin between thumbnails'] = 'Интервал между миниатюрами';
+$lang['Double the size of the first thumbnail'] = 'Двойной размер первой миниатюры';
+$lang['Cache the big thumbnails (recommended)'] = 'Кэшировать большие миниатюры (рекомендуется)';
+$lang['Scale thumbnails'] = 'Масштабировать миниатюры';
+$lang['Cache Informations'] = 'Кэшировать данные';
+$lang['Purge thumbnails cache'] = 'Очистить кэш миниатюр';
+$lang['%d file'] = '%й файл';
+$lang['%d files'] = '% файлов';
+$lang['Cache have been generated'] = 'Кэш записан';
+$lang['Pre-cache thumbnails'] = 'Предварительное кэширование миниатюр';
+$lang['Delete images in GThumb+ cache.'] = 'Удалить фотографии из кэша GThumb+.';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Найти фотографии, которые не были кэшированы и провести кэширование для них.';
+
+?>
