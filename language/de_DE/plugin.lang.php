4c6ddc2aec7eeaca031bdf20765fb39a94a5ecc4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-01-17 19:01:43 +0000

    [GThumb+] Update de_DE, thanks to : Sugar888
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@12915 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -2,4 +2,17 @@
 
 
 
+$lang['%d file'] = '%d Datei';
+$lang['%d files'] = '%d Dateien';
+$lang['Cache have been generated'] = 'Zwischenspeicher wurde erzeugt';
+$lang['Cache Informations'] = 'Informationen zum Zwischenspeicher';
+$lang['Cache the big thumbnails (recommended)'] = 'Die großen Thumbnails im Zwischenspeicher aufnehmen (empfohlen)';
+$lang['Delete images in GThumb+ cache.'] = 'Löscht Bilder im GThumb+ Zwischenspeicher.';
+$lang['Double the size of the first thumbnail'] = 'Doppelt so groß wie der erste Thumbnail';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Findet Bilder, die nicht zwischengespeichert wurden und erstellt die Cache-Version.';
+$lang['Margin between thumbnails'] = 'Abstand zwischen den Thumbnails';
+$lang['Pre-cache thumbnails'] = 'Pre-cache Thumbnails';
+$lang['Purge thumbnails cache'] = 'Thumbnails-Zwischenspeicher leeren';
+$lang['Scale thumbnails'] = 'Thumbnails skalieren';
+$lang['Thumbnails max height'] = 'Maximal Höhe der Thumbnails';
 ?>
