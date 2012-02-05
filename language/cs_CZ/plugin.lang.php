afbc0f19bb54bd082031cc729627e3fc9f3707cd
ddtddt <ddtddt@piwigo.org>
2012-02-05 07:57:51 +0000

    [extensions] - GThumb - add cs_CZ - Česky – Czech - Thanks to webprostor.eu
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@13032 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['Thumbnails max height'] = 'Max výška náhledu';
+$lang['Margin between thumbnails'] = 'Margin mezi náhledy';
+$lang['Double the size of the first thumbnail'] = 'Zdvojit velikost prvního náhledu';
+$lang['Cache the big thumbnails (recommended)'] = 'Cachovat velké náhledy (doporučeno)';
+$lang['Scale thumbnails'] = 'Rozměr náhledů';
+$lang['Cache Informations'] = 'Cachovat Informace';
+$lang['Purge thumbnails cache'] = 'Vyčistit cache náhledů';
+$lang['%d file'] = '%d soubor';
+$lang['%d files'] = '%d souborů';
+$lang['Cache have been generated'] = 'Cache byla vygenerována';
+$lang['Pre-cache thumbnails'] = 'Před-cache náhledů';
+$lang['Delete images in GThumb+ cache.'] = 'Odstranit obrázky v GThumb a cache.';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Najít obrázky, které nebyly dosud uloženy v cache a vytvořit jejich cache verzi.';
+
+?>
