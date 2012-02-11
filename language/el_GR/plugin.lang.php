4bc6a5a7f13ffd5d90e13fcf5e856ff50e942888
ddtddt <ddtddt@piwigo.org>
2012-02-11 09:33:59 +0000

    [extensions] - GThumb - el_GR- Greek - ελληνικά - Thanks to Lamprianidis
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@13095 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['Thumbnails max height'] = 'Mέγιστο ύψος μικρογραφιών';
+$lang['Margin between thumbnails'] = 'Απόσταση μεταξύ των μικρογραφιώνs';
+$lang['Double the size of the first thumbnail'] = 'Διπλό μέγεθος της πρώτης μικρογραφίας';
+$lang['Cache the big thumbnails (recommended)'] = 'Cache στις μεγάλες μικρογραφίες (προτείνεται)';
+$lang['Scale thumbnails'] = 'Κλίμακα μικρογραφιών';
+$lang['Cache Informations'] = 'Πληροφορίες Cache';
+$lang['Purge thumbnails cache'] = 'Εκκαθάριση cache μικρογραφιών';
+$lang['%d file'] = '%d αρχείο';
+$lang['%d files'] = '%d αρχεία';
+$lang['Cache have been generated'] = 'H Cache έχει δημιουργηθεί';
+$lang['Pre-cache thumbnails'] = 'Pre-cache μικρογραφίες';
+$lang['Delete images in GThumb+ cache.'] = 'Διαγραφή εικόνων στο GThumb+ cache.';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Εύρεση εικόνων που δεν έχουν γίνει cach και δημιουργία έκσοδης cache'; 
+
+?>
