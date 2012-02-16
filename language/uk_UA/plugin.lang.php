745f8fe291c98795c60bbbe990df29928d8d9fca
ddtddt <ddtddt@piwigo.org>
2012-02-16 21:26:02 +0000

    [extensions] - stripped-slide - add uk_UA -  Українська -Ukrainian  - Thanks to quest_tom
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@13198 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Thumbnails max height'] = 'Максимальна висота ескізу';
+$lang['Margin between thumbnails'] = 'Відстань між ескізами';
+$lang['Double the size of the first thumbnail'] = 'Подвоїти розмір першого ескізу';
+$lang['Cache the big thumbnails (recommended)'] = 'Кешувати великі ескізи (рекомендовано)';
+$lang['Scale thumbnails'] = 'Масштаб ескізу';
+$lang['Cache Informations'] = 'Кешувати інформацію';
+$lang['Purge thumbnails cache'] = 'Очистити кеш ескізів';
+$lang['%d file'] = '%d файл';
+$lang['%d files'] = '%d файли';
+$lang['Cache have been generated'] = 'Кеш був згенерований';
+$lang['Pre-cache thumbnails'] = 'Попередньо кешувати ескізи';
+$lang['Delete images in GThumb+ cache.'] = 'Видалити зображення в GThumb+ кеші.';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Пошук зображень, яких немає в кеші, і створення версії в кеші.';
+?>
