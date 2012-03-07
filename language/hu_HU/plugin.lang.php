add0e501f3d3906d804e5982ea43403fb3df56bd
ddtddt <ddtddt@piwigo.org>
2012-03-07 14:36:59 +0000

    [extensions] - GThumb - add hu_HU - Magyar – Hungarian - Thanks to evryka23
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@13510 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Thumbnails max height'] = 'Bélyegképek max. magassága';
+$lang['Margin between thumbnails'] = 'Bélyegképek közötti szegély';
+$lang['Double the size of the first thumbnail'] = 'Első bélyegkép dupla mérete';
+$lang['Cache the big thumbnails (recommended)'] = 'Nagy bélyegképek tárolása (ajánlott)';
+$lang['Scale thumbnails'] = 'Bélyegképek méretezése';
+$lang['Cache Informations'] = 'Információk tárolása';
+$lang['Purge thumbnails cache'] = 'Tárolt bélyegképek tisztítása';
+$lang['%d file'] = '%t fájl';
+$lang['%d files'] = '%t fájlok';
+$lang['Cache have been generated'] = 'Gyorsítótár létrehozva';
+$lang['Pre-cache thumbnails'] = 'Bélyegképek előretárolása';
+$lang['Delete images in GThumb+ cache.'] = 'Képek törlése a GThumb+ cache.-ban';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Nem tárolt képek keresése és tároló verzió létrehozása';
+?>
