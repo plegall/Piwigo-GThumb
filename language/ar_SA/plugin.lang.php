0db73e1c89c7939f86401aa09998ea0f3d60c895
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-27 18:45:19 +0000

    [gthumb] Update ar_SA, thanks to : mohammed
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@14483 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d ملف';
+$lang['%d files'] = '%d ملفات';
+$lang['Cache Informations'] = ' معلومات التخزين المؤقت';
+$lang['Cache have been generated'] = 'تم انشاء مساحة للتخزين';
+$lang['Cache the big thumbnails (recommended)'] = 'تخزين الصورة المصغر الكبيرة مؤقتا ( مُستحسن )';
+$lang['Delete images in GThumb+ cache.'] = 'خذف الصور المصغرة في المخزن  GThub  الموقته';
+$lang['Double the size of the first thumbnail'] = 'مضاعفة حجم الصور المصغرة';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'ابحث عن الصور التي لم تخزن مؤقتا و انشأ لها مساحة مؤقته للتخزين';
+$lang['Margin between thumbnails'] = 'الهامش بين الصور المصغرة';
+$lang['Pre-cache thumbnails'] = 'اعادة تخزين مؤقت للصور المصغرة';
+$lang['Purge thumbnails cache'] = ' مسح الصورة المخزنة مؤقتا ';
+$lang['Scale thumbnails'] = 'مقياس الصور المصغرة';
+$lang['Thumbnails max height'] = 'أقصى ارتفاع للصور المصغرة';
+?>
