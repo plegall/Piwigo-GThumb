2222e2df8ff34a05af378532902a67281e5ed4f0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-03 12:14:03 +0000

    [gthumb] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@14703 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d arxiu';
+$lang['%d files'] = '%d arxius';
+$lang['Cache Informations'] = 'Informació de memòria cau';
+$lang['Cache have been generated'] = 'La memòria cau ha sigut generada';
+$lang['Cache the big thumbnails (recommended)'] = 'Emmagatzemar en memòria cau les miniatures grans (recomanat)';
+$lang['Delete images in GThumb+ cache.'] = 'Eliminar les imatges a GThumb i a la memòria cau';
+$lang['Double the size of the first thumbnail'] = 'Doblar la mida de la primera miniatura';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Trobar imatges que no s\'han emmagatzemat a la memòria cau i crear la versió en memòria cau.';
+$lang['Margin between thumbnails'] = 'Marge entre les miniatures';
+$lang['Pre-cache thumbnails'] = 'Miniatures en pre-memoria cau';
+$lang['Purge thumbnails cache'] = 'Buidar memòria cau de miniatures';
+$lang['Scale thumbnails'] = 'Escalar miniatures';
+$lang['Thumbnails max height'] = 'Alçada màxima de les miniatures';
+?>
