db369e4646ef53766a1bdb03731e139310003a41
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-26 20:29:27 +0000

    [gthumb] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@16066 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d فایل';
+$lang['%d files'] = '%d فایل';
+$lang['Cache Informations'] = 'اطلاعات کش (Cache)';
+$lang['Cache have been generated'] = 'کش (Cache) تولید شده است';
+$lang['Cache the big thumbnails (recommended)'] = 'کش (Cache) کردن عکس ریزه های بزرگ (توصیه می شود)';
+$lang['Delete images in GThumb+ cache.'] = 'پاک کردن کش (Cache) عکس ها در GThumb+.';
+$lang['Double the size of the first thumbnail'] = 'دو برابر کردن سایز عکس ریزه ی نخست';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'پیدا کردن عکس هایی که کش (Cache) نشده اند و ساخت یک نگارش کش شده از آن ها.';
+$lang['Margin between thumbnails'] = 'فاصله ی بین عکس ریزه ها';
+$lang['Pre-cache thumbnails'] = 'بررسی پیش از کش (Cache) کردن عکس ریزه ها';
+$lang['Purge thumbnails cache'] = 'تخلیه کردن کش (Cache) عکس ریزه ها';
+$lang['Scale thumbnails'] = 'بزرگنمایی عکس ریزه ها';
+$lang['Thumbnails max height'] = 'بیشترین بلندای عکس ریزه ها';
+?>
