046966740e9a5ebaa98cbae583f43464b795d994
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-29 21:36:14 +0000

    [gthumb] Update nb_NO, thanks to : skaulen
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@16183 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d fil';
+$lang['%d files'] = '%d filer';
+$lang['Cache Informations'] = 'Mellomlager informasjon';
+$lang['Cache have been generated'] = 'Mellomlager er opprettet';
+$lang['Cache the big thumbnails (recommended)'] = 'Mellomlagre de store miniatyrbildene(anbefalt)';
+$lang['Delete images in GThumb+ cache.'] = 'Slett bildene i GThumb+mellomlager';
+$lang['Double the size of the first thumbnail'] = 'Dobler størrelsen på det første miniatyrbildet';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Finner bilder som ikke har blitt mellomlagret og lager en mellomlagret versjon';
+$lang['Margin between thumbnails'] = 'Margin mellom miniatyrbildene';
+$lang['Pre-cache thumbnails'] = 'Mellomlagre miniatyrbilder';
+$lang['Purge thumbnails cache'] = 'Slett miniatyrbildemellomlager';
+$lang['Scale thumbnails'] = 'Skaler miniatyrbilder';
+$lang['Thumbnails max height'] = 'Max høyde miniatyrbilder';
+?>
