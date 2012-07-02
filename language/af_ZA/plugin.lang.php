dd6b658fe733800cf858bd7785c749647e76ab02
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-02 19:24:40 +0000

    [gthumb] Update af_ZA, thanks to : maeztro
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@16283 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/af_ZA/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d lêer';
+$lang['%d files'] = '%d lêers';
+$lang['Cache Informations'] = 'Spoedkas inligting';
+$lang['Cache have been generated'] = 'Spoedkas is geskep';
+$lang['Cache the big thumbnails (recommended)'] = 'Spoedkas die groot duimnaels (aanbeveel)';
+$lang['Delete images in GThumb+ cache.'] = 'Verwyder beelde in GThumb+ se spoedkas';
+$lang['Double the size of the first thumbnail'] = 'Verdubbel die grootte van die eerste duimnael';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Vind beelde wat nog nie in die spoedkas is nie en skep dit.';
+$lang['Margin between thumbnails'] = 'Grense tussen duimnaels';
+$lang['Pre-cache thumbnails'] = 'Voorskep duimnaels';
+$lang['Purge thumbnails cache'] = 'Maak duimnael spoedkas leeg';
+$lang['Scale thumbnails'] = 'Skaal duimnaels';
+$lang['Thumbnails max height'] = 'Duimnaels maksimum hoogte';
+?>
