3ea8d48d32196e2ab836dad8abe66c24800f361a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-01 15:40:55 +0000

    [gthumb] Update bg_BG, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@17280 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d файл';
+$lang['%d files'] = '%d файлове';
+$lang['Cache Informations'] = 'Информация за кеширането';
+$lang['Cache have been generated'] = 'Кеширането е генерирано';
+$lang['Cache the big thumbnails (recommended)'] = 'Кеширане на големите миниатюри (препоръчително)';
+$lang['Delete images in GThumb+ cache.'] = 'Изтриване на изображенията в GThumb+ кеша.';
+$lang['Double the size of the first thumbnail'] = 'Двоен размер на първата миниатюра';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Намиране на изображения, които не са кеширани и създава кеш версия.';
+$lang['Margin between thumbnails'] = 'Място между миниатюрите';
+$lang['Thumbnails max height'] = 'Максимална височина на миниатюрите';
+$lang['Scale thumbnails'] = 'Мащаб на миниатюрите';
+$lang['Purge thumbnails cache'] = 'Почистване на кеша с миниатюри';
+$lang['Pre-cache thumbnails'] = 'Презареждане на миниатюрите';
+?>
