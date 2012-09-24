9c5cfa19364bbf62c91f26e8db3967890a8e9275
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-24 04:32:33 +0000

    [gthumb] Update th_TH, thanks to : pradit
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@18194 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/th_TH/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Pre-cache thumbnails'] = 'รูปขนาดย่อก่อนแคช';
+$lang['Purge thumbnails cache'] = 'ล้างแคชรูปขนาดย่อ';
+$lang['Scale thumbnails'] = 'มาตราส่วน ภาพขนาดเล็ก';
+$lang['Show thumbnails caption'] = 'แสดงคำอธิบายภาพขนาดเล็ก';
+$lang['Thumbnails max height'] = 'ความสูงสูงสุดของภาพขนาดเล็ก';
+$lang['Margin between thumbnails'] = 'ขอบระหว่างภาพขนาดเล็ก';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'พบภาพที่ไม่ได้ถูกเก็บไว้และสร้างรุ่นแคช';
+$lang['Double the size of the first thumbnail'] = 'สองเท่าของขนาดของภาพขนาดย่อก่อน';
+$lang['Delete images in GThumb+ cache.'] = 'ลบภาพใน GThumb + แคช';
+$lang['Cache Informations'] = 'ข้อมูลแคช';
+$lang['Cache have been generated'] = 'แคชได้รับการสร้าง';
+$lang['Cache the big thumbnails (recommended)'] = 'แคชรูปขนาดย่อขนาดใหญ่ (แนะนำ)';
+$lang['%d file'] = '%d ไฟล์';
+$lang['%d files'] = '%d ไฟล์';
+?>
