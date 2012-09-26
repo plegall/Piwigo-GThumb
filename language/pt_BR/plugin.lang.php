e04017ce0adbe64fc8d7b9f03bebbb35501f3410
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-26 19:23:55 +0000

    [gthumb] Update pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@18287 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Scale thumbnails'] = 'Escala das miniaturas';
+$lang['%d file'] = '%d arquivo';
+$lang['%d files'] = '%d arquivos';
+$lang['Cache Informations'] = 'Informações de Cache';
+$lang['Cache have been generated'] = 'Cache foi gerado';
+$lang['Cache the big thumbnails (recommended)'] = 'Cria cache das grandes miniaturas (recomendado)';
+$lang['Delete images in GThumb+ cache.'] = 'Apaga imagens no GThumb+ cache';
+$lang['Double the size of the first thumbnail'] = 'Dobra o tamanho da primeira miniatura';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Encontre imagens que não tenham sido cacheadas e cria versão em cache.';
+$lang['Margin between thumbnails'] = 'Margem entre miniaturas';
+$lang['Pre-cache thumbnails'] = 'Pré-cache das mininiaturas';
+$lang['Purge thumbnails cache'] = 'Limpa cache das miniaturas';
+$lang['Show thumbnails caption'] = 'Mostre título das miniaturas';
+$lang['Thumbnails max height'] = 'Altura máxima das miniaturas';
+?>
