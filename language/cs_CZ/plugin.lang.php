b46b87904b07d1bbc5d56dc87d88e26572edc19f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-29 06:01:17 +0000

    [gthumb] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@18356 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/cs_CZ/plugin.lang.php
+++ language/cs_CZ/plugin.lang.php
@@ -14,4 +14,5 @@ $lang['Pre-cache thumbnails'] = 'Před-cache náhledů';
 $lang['Delete images in GThumb+ cache.'] = 'Odstranit obrázky v GThumb a cache.';
 $lang['Finds images that have not been cached and creates the cached version.'] = 'Najít obrázky, které nebyly dosud uloženy v cache a vytvořit jejich cache verzi.';
 
+$lang['Show thumbnails caption'] = 'Zobrazit titulek náhledů';
 ?>
