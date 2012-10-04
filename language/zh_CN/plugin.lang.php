86a730d5749788117cf49a38f3daf0f952bf514b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-04 17:29:27 +0000

    [gthumb] Update zh_CN, thanks to : winson
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@18469 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -14,4 +14,5 @@ $lang['Pre-cache thumbnails'] = '预缓存缩略图';
 $lang['Delete images in GThumb+ cache.'] = '删除GThumb+缓存中的图片.';
 $lang['Finds images that have not been cached and creates the cached version.'] = '某些图片无法被缓存并创建缓存版本.';
 
+$lang['Show thumbnails caption'] = '显示缩略图标题';
 ?>
