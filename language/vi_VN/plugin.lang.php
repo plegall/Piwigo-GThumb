4ff65aa71ca64fd971a8a9ca906c1ffcd83b9f8a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-07 04:36:04 +0000

    [gthumb] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/GThumb@28104 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d file';
+$lang['%d files'] = '%d files';
+$lang['Cache Informations'] = 'Thông tin cache';
+$lang['Cache have been generated'] = 'Cache đã được tạo';
+$lang['Cache the big thumbnails (recommended)'] = 'Cache các thumbnail lớn (đề nghị)';
+$lang['Delete images in GThumb+ cache.'] = 'Xóa ảnh trong GThumb+ cache.';
+$lang['Double the size of the first thumbnail'] = 'Tăng gấp đôi kích thước của thumbnail đầu tiên';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Tìm các ảnh chưa được cache và tạo phiên bản cache.';
+$lang['Margin between thumbnails'] = 'Lề giữa các thumbnail';
+$lang['Pre-cache thumbnails'] = 'Thumbnail pre-cache';
+$lang['Purge thumbnails cache'] = 'Xóa thumbnail cache';
+$lang['Scale thumbnails'] = 'Co giãn thumbnail';
+$lang['Show thumbnails caption'] = 'Hiện tiêu đề thumbnail';
+$lang['Thumbnails max height'] = 'Chiều cao tối đa thumbnail';
