1da0a559edb3557c0a26dbfd94b5f7ff7bef141f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-06 09:46:00 +0100

    Update de_DE, thanks to bigant

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -4,16 +4,16 @@
 
 $lang['%d file'] = '%d Datei';
 $lang['%d files'] = '%d Dateien';
-$lang['Cache have been generated'] = 'Zwischenspeicher wurde erzeugt';
+$lang['Cache have been generated'] = 'Zwischenspeicher wurde generiert';
 $lang['Cache Informations'] = 'Informationen zum Zwischenspeicher';
-$lang['Cache the big thumbnails (recommended)'] = 'Die großen Thumbnails im Zwischenspeicher aufnehmen (empfohlen)';
-$lang['Delete images in GThumb+ cache.'] = 'Löscht Bilder im GThumb+ Zwischenspeicher.';
-$lang['Double the size of the first thumbnail'] = 'Doppelt so groß wie der erste Thumbnail';
-$lang['Finds images that have not been cached and creates the cached version.'] = 'Findet Bilder, die nicht zwischengespeichert wurden und erstellt die Cache-Version.';
-$lang['Margin between thumbnails'] = 'Abstand zwischen den Thumbnails';
-$lang['Pre-cache thumbnails'] = 'Pre-cache Thumbnails';
-$lang['Purge thumbnails cache'] = 'Thumbnails-Zwischenspeicher leeren';
-$lang['Scale thumbnails'] = 'Thumbnails skalieren';
-$lang['Thumbnails max height'] = 'Maximal Höhe der Thumbnails';
-$lang['Show thumbnails caption'] = 'Zeige Beschreibung der Thumbnails';
-?>
+$lang['Cache the big thumbnails (recommended)'] = 'Große Vorschaubilder zwischenspeichern (empfohlen)';
+$lang['Delete images in GThumb+ cache.'] = 'Bilder aus dem GThumb+ Zwischenspeicher löschen';
+$lang['Double the size of the first thumbnail'] = 'Doppelt so groß wie das erste Vorschaubild';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Bilder finden, die nicht zwischengespeichert wurden und Zwischenspeicher-Version erstellen';
+$lang['Margin between thumbnails'] = 'Abstand zwischen den Vorschaubildern';
+$lang['Pre-cache thumbnails'] = 'Vorschaubilder zwischenspeichern';
+$lang['Purge thumbnails cache'] = 'Vorschaubilder-Zwischenspeicher leeren';
+$lang['Scale thumbnails'] = 'Vorschaubilder skalieren';
+$lang['Thumbnails max height'] = 'Maximal Höhe der Vorschaubilder';
+$lang['Show thumbnails caption'] = 'Bildunterschriften der Vorschaubilder anzeigen';
+$lang['Show rating score in caption'] = 'Bewertung in Bildunterschrift anzeigen';
