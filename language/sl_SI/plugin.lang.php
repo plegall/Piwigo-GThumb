c9b03a17b8f66ab0cd22efa03016b35eccef607c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 08:54:34 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Show rating score in caption'] = 'Prikaži oceno v napisu';
+$lang['Scale thumbnails'] = 'Prilagodite sličice';
+$lang['Cache Informations'] = 'Informacije o predpomnilniku';
+$lang['Cache have been generated'] = 'Predpomnilnik je bil ustvarjen';
+$lang['Cache the big thumbnails (recommended)'] = 'Predpomnilnik velike sličice (priporočeno)';
+$lang['Delete images in GThumb+ cache.'] = 'Izbrišite slike v predpomnilniku GDThumb+.';
+$lang['Double the size of the first thumbnail'] = 'Podvojite velikost prve sličice';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'Poišče slike, ki niso bile predpomnjene, in ustvari predpomnjeno različico.';
+$lang['Margin between thumbnails'] = 'Rob med sličicami';
+$lang['Pre-cache thumbnails'] = 'Predpomnilnik sličic';
+$lang['Purge thumbnails cache'] = 'Počisti predpomnilnik sličic';
+$lang['Show thumbnails caption'] = 'Prikaži napis sličic';
+$lang['Thumbnails max height'] = 'Največja višina sličic mora biti celo število';
+$lang['%d file'] = '%d datoteka';
+$lang['%d files'] = '%d datoteke';
