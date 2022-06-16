24b8dfcaa023b14eff47aed4155da11f36e5d0d9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-16 20:51:27 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,11 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d skrá';
+$lang['%d files'] = '%d skrár';
+$lang['Cache Informations'] = 'Upplýsingar um skyndiminni';
+$lang['Cache have been generated'] = 'Skyndiminni hefur verið búið til';
