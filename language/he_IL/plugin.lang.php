0ef9f657edb2f694fdd2cd4a71cee006709803dc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:16:26 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d קובץ';
+$lang['%d files'] = '%d קבצים';
+$lang['Cache Informations'] = 'מידע מטמון';
+$lang['Cache have been generated'] = 'המטמון נוצר';
+$lang['Cache the big thumbnails (recommended)'] = 'מטמן את התמונות הקטנות הגדולות (מומלץ)';
+$lang['Delete images in GThumb+ cache.'] = 'מחק תמונות במטמון של GThumb+';
+$lang['Double the size of the first thumbnail'] = 'הכפל את גודל התמונה הראשונה';
+$lang['Finds images that have not been cached and creates the cached version.'] = 'מוצא תמונות שלא נמטמנו ויוצר את הגרסה המוטמנת';
+$lang['Margin between thumbnails'] = 'שול בין התמונות הקטנות';
+$lang['Pre-cache thumbnails'] = 'מטמן מראש תמונות קטנות';
+$lang['Purge thumbnails cache'] = 'ניקוי מטמון תמונות קטנות';
+$lang['Scale thumbnails'] = 'מידול תמונות קטנות';
+$lang['Show rating score in caption'] = 'הצג ציון דירוג בתיאור';
+$lang['Show thumbnails caption'] = 'הצג תיאורי תמונות קטנות';
+$lang['Thumbnails max height'] = 'גובה מרבי של תמונות קטנות';
