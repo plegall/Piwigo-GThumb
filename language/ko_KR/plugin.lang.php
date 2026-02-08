a82048c7fe093d4ca527bfe6041e648b02a5c21e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-08 08:14:11 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d file'] = '%d 개 파일';
+$lang['%d files'] = '%d 개 파일';
+$lang['Cache Informations'] = '캐시 정보';
+$lang['Cache have been generated'] = '캐시가 생성되었습니다';
+$lang['Cache the big thumbnails (recommended)'] = '큰 썸네일 캐시 (권장)';
+$lang['Delete images in GThumb+ cache.'] = 'GThumb+ 캐시의 이미지 삭제.';
+$lang['Double the size of the first thumbnail'] = '첫 번째 썸네일 크기 두 배로 설정';
+$lang['Finds images that have not been cached and creates the cached version.'] = '캐시되지 않은 이미지 검색 및 캐시 버전 생성';
+$lang['Margin between thumbnails'] = '썸네일 간 여백';
+$lang['Pre-cache thumbnails'] = '썸네일 사전 캐시';
+$lang['Purge thumbnails cache'] = '썸네일 캐시 정리';
+$lang['Scale thumbnails'] = '썸네일 크기 조정';
+$lang['Show rating score in caption'] = '캡션에 평점 표시';
+$lang['Show thumbnails caption'] = '썸네일 캡션 표시';
+$lang['Thumbnails max height'] = '썸네일 최대 높이';
